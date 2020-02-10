6eec31873a56f62f522a416f1a4b78038f9fc47a
Jonathan Joe <JonathanDonJoe@gmail.com>
2020-02-10 11:05:32 -0500

    Implements bare-bones redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// console.log('working')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function counter(state = { amount: 100 }, action) {
+    console.table(action)
+    const newState = { ...state };
+    if (action.type === 'INCREMENT') {
+        newState.amount = state.amount + 1
+    } else if (action.type === 'DECREMENT') {
+        newState.amount = state.amount - 1
+    } else {
+
+    }
+    return newState
+}
+
+const store = createStore(counter);
+
+store.subscribe(() => {
+    console.log(`The state is now`)
+    console.table(store.getState())
+})
+
+store.dispatch({
+    type: 'INCREMENT'
+})
+store.dispatch({
+    type: 'INCREMENT'
+})
+store.dispatch({
+    type: 'DECREMENT'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
